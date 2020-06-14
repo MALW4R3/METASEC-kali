b47ebfc65c2e4f18fdffd017fb277a28519cde8d
APT-GET47 <57804710+MALW4R3@users.noreply.github.com>
2020-06-14 16:27:30 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo mv update-metasec /bin
+cd
+cd ..
+cd ..
+cd bin
+sudo chmod +x update-metasec
+cd
+cd METASEC
+unzip ngrok.zip
+sudo mv ngrok /$HOME
+rm -rf ngrok.zip
+clear
+sudo mv metasec /bin
+cd
+cd ..
+cd ..
+cd bin
+sudo chmod +x *
+cd
+cd METASEC
+clear
+cat g.txt | lolcat

